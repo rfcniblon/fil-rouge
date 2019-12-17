e633abd54d434a1473b2473fb3ce2d52bc0df541
rfcniblon <51241155+rfcniblon@users.noreply.github.com>
2019-12-17 13:49:07 +0100

    Update conf.js

--- conf.js
+++ conf.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection(
     {
         host:"localhost",
         user:"root",
-        password:"dzen4jane",
+        password:"XXXXXXX",
         database:"tim_burton"
     }
 );
